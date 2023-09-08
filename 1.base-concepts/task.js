1ae65ab61fe8f3ed8c926e2b0cd69b6a6e0f7ce3
alezzzka <120045846+alezzzka@users.noreply.github.com>
2023-09-08 07:51:29 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,28 +18,18 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-  percent = +percent;
-  contribution = +contribution;
-  amount = +amount;
-  date = +date;
-  if (typeof percent === `string`) {
-    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
-  } else if (typeof contribution === `string`) {
-    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
-  } else if (typeof amount === `string`) {
-    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
-  } else if (typeof date === `string`) {
-    totalAmount = `Параметр "Сроки ипотеки" содержит неправильное значение ${date}`;
-  };
-  let lianAmount = amount - contribution;
-  let timeNow = new Date();
-  let loanTerm = date - timeNow;
-  loanTerm = loanTerm / 1000 / 60 / 60 / 24 / 30;
-  let monthlyPartPercent = (percent / 100) / 12;
-  let monthFee = lianAmount * (monthlyPartPercent + (monthlyPartPercent / (((1 + monthlyPartPercent) ** loanTerm) - 1)));
-  totalAmount = +(monthFee * loanTerm).toFixed(2);
-  console.log(totalAmount);
-  return totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+    return false;
+  }
+
+  percent = percent/100/12;
+
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+
+  totalPayment = Number(totalPayment.toFixed(2));
+
+  return totalPayment;
 }
