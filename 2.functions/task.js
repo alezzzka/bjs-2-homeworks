173f09726e470fb2defbe8e8605943e218e98948
alezzzka <120045846+alezzzka@users.noreply.github.com>
2023-09-10 17:54:29 +0300

    task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,47 @@
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
-}
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
+
+
+	for (i = 0; i < arr.length; i++) {
+
+		if (arr[i] !== undefined) {
+
+			if (max < arr[i]) {
+				max = arr[i];
+			};
+
+			if (min > arr[i]) {
+				min = arr[i];
+			};
+
+			if (typeof arr[i] === "number" && arr[i] !== NaN) {
+				sum += arr[i];
+			};
+
+			avg = sum / arr.length;
+
+		} else {
+			return 0;
+		};
+	};
+
+	return {
+		min: min,
+		max: max,
+		avg: Number(avg.toFixed(2))
+	};
+
+};
+
+getArrayParams(10, 10, 11, 20, 10);
+
+
+
+
 
 function summElementsWorker(...arr) {
 
